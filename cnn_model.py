a9b0212f4efe720e143981f863df802193f2620f
Awesome_Tang <13828853483@163.com>
2019-01-26 16:08:03 +0800

    Model definition🌈🌈🌈

--- cnn_model.py
+++ cnn_model.py
@@ -50,21 +50,18 @@ class CNN:
         b_cv1 = self.bias_variable([32])
         h_cv1 = tf.nn.relu(self.conv2d(x_norm, w_cv1) + b_cv1)
         h_mp1 = self.max_pool_2x2(h_cv1)
-        #h_mp1 = tf.nn.dropout(h_mp1,Config.keep_prob)
 
         # 卷积层 2
         w_cv2 = self.weight_variable([5, 5, 32, 64])
         b_cv2 = self.bias_variable([64])
         h_cv2 = tf.nn.relu(self.conv2d(h_mp1, w_cv2) + b_cv2)
         h_mp2 = self.max_pool_2x2(h_cv2)
-        #h_mp2 = tf.nn.dropout(h_mp2, Config.keep_prob)
 
         # 卷积层 3
         w_cv3 = self.weight_variable([5, 5, 64, 64])
         b_cv3 = self.bias_variable([64])
         h_cv3 = tf.nn.relu(self.conv2d(h_mp2, w_cv3) + b_cv3)
         h_mp3 = self.max_pool_2x2(h_cv3)
-        # h_mp3 = tf.nn.dropout(h_mp3, Config.keep_prob)
 
         # 全连接
         W_fc1 = self.weight_variable([20 * 8 * 64, 128])
@@ -84,9 +81,10 @@ class CNN:
                                       len(Config.characters)], name='predict')
         labels = tf.reshape(self.input_y, [-1, Config.char_num,
                                            len(Config.characters)], name='labels')
-        predict_max_idx = tf.argmax(predict, axis=2, name='predict_max_idx')
+
+        self.predict_max_idx = tf.argmax(predict, axis=2, name='predict_max_idx')
         labels_max_idx = tf.argmax(labels, axis=2, name='labels_max_idx')
-        predict_correct_vec = tf.equal(predict_max_idx, labels_max_idx)
+        predict_correct_vec = tf.equal(self.predict_max_idx, labels_max_idx)
 
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(update_ops):
